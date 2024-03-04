5243018827c0a8eb58eb9abc1de21b73fcc6fc94
Alyssa Koh <a9koh@stanford.edu>
2024-03-04 14:47:16 -0800

    added classes for title of list and for each task. made separate task component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,26 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+
+import '../index.css';
+
+function Task() {
+    return (
+        <div className="square">
+            Here is a task! motherfucker
+        </div>
+    );
+}
 
 export default function List() {
     return (
-        <div>
-            meow
-        </div>
+        <>
+            <div className="list-title">
+                Your fuckin TODO list bitch
+            </div>
+            <Task />
+            <Task />
+            {/* <ol>
+                {tasks}
+            </ol> */}
+        </>
     );
 }
