1e98e57dd100e695fa89953d5c438d70c007e5ca
Alyssa Koh <a9koh@stanford.edu>
2024-03-04 19:48:36 -0800

    added 'desc' prop for Task component; added state for list of tasks so that the page re-renders when a new task is added using the button

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,23 +1,36 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 import Task from './Task';
 
 import '../index.css';
 
 export default function List() {
+    const [taskInfo, setTaskInfo] = useState([]);
+
+    function handleAdd() {
+        const newTaskInfo = [...taskInfo, 'new thing'];
+        setTaskInfo(newTaskInfo);
+    }
+
+    const tasks = taskInfo.map((cur_desc, index) => {
+        return (
+            <Task key={index} desc={cur_desc}/>
+        )
+    });
+
     return (
         <>
             <div className="list-title">
                 <div>
                     Your fuckin TODO list bitch
                 </div>
-                <button>Add a task bitch</button>
+                <button onClick={handleAdd} >Add a task bitch</button>
             </div>
-            <Task />
-            <Task />
-            {/* <ol>
+            {/* <Task desc={template_desc} />
+            <Task /> */}
+            <ol>
                 {tasks}
-            </ol> */}
+            </ol>
         </>
     );
 }
