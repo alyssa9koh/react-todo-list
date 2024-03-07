d2684b98d5d62d1964789f01d14098b5eea2af8f
alyssa9koh <a9koh@stanford.edu>
2024-03-07 14:14:07 -0800

    added delete button and updated styling (#5)

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,20 +2,27 @@ import { useState } from 'react';
 
 import '../index.css';
 
-export default function Task({ desc }) {
+export default function Task({ desc, onDelete }) {
     const [isDone, setIsDone] = useState(false);
 
-    function handleDoneClick() {
+    function handleDone() {
         setIsDone(!isDone);
+    }
+
+    function handleDelete() {
+       onDelete();
     }
 
     return (
         <div className="square">
-            <div className="done-button" onClick={handleDoneClick}>
+            <div className="done-button" onClick={handleDone}>
                 {isDone ? 'X' : ''}
             </div>
-            <div>
+            <div className="desc">
                 { desc }
+            </div>
+            <div className="delete-button" onClick={handleDelete}>
+                delete
             </div>
         </div>
     );
